83c6f210ac86b81860e8eca9542a059fad1a7f34
Aniket <aniketicloud@gmail.com>
2020-10-20 10:33:00 +0530

    first commit

---
+++ src/components/Skeleton/Farm/Web/IndexSkeleton.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row no-gutters>
+    <v-col>
+      <skeleton-sheet></skeleton-sheet>
+    </v-col>
+    <v-col>
+      <skeleton-sheet></skeleton-sheet>
+    </v-col>
+    <v-col>
+      <skeleton-sheet></skeleton-sheet>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import SkeletonSheet from "./SkeletonSheet";
+export default {
+  components: {
+    SkeletonSheet,
+  },
+};
+</script>
---
+++ src/components/Skeleton/Farm/Web/SkeletonSheet.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-sheet
+      color="red"
+      width="404"
+      height="566"
+      tile
+      class="mb-8 hidden-sm-and-down"
+    >
+      <header-skeleton></header-skeleton>
+      <text-skeleton></text-skeleton>
+      <text-skeleton></text-skeleton>
+      <text-skeleton></text-skeleton>
+      <text-skeleton></text-skeleton>
+    </v-sheet>
+
+    <v-sheet
+      color="red"
+      width="404"
+      height="316"
+      tile
+      class="mb-8 hidden-sm-and-down"
+    >
+      <header-skeleton></header-skeleton>
+      <text-skeleton></text-skeleton>
+      <text-skeleton></text-skeleton>
+    </v-sheet>
+  </div>
+</template>
+
+<script>
+import TextSkeleton from "./TextSkeleton";
+import HeaderSkeleton from "./HeaderSkeleton";
+export default {
+  components: {
+    TextSkeleton,
+    HeaderSkeleton,
+  },
+};
+</script>
